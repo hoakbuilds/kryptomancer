0aa946ab37c86c6f59f641c0746f6aeed2877023
Hugo Carvalho <warderld@hotmail.com>
2018-11-20 03:34:37 +0000

    adicionei tabela para mostrar ficheiros encriptados e botoes para adicionar funcionalidades

--- kryptoflask/routes/routes.py
+++ kryptoflask/routes/routes.py
@@ -48,7 +48,16 @@ def crypter():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            return redirect(request.url)
+            files, enc = get_uploaded_files()
+            selected_file = request.form.get('files')
+            if selected_file == None:
+                print('why')
+                print(request.form['selected_files'], file=sys.stderr)
+                return render_template('file_crypter.html', listdir=os.listdir(UPLOAD_FOLDER), enc_files=enc)
+            else:
+                print('wtf')
+                print(selected_file, file=sys.stderr)
+            return render_template('file_crypter.html', listdir=os.listdir(UPLOAD_FOLDER), enc_files=enc)
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
@@ -57,9 +66,11 @@ def crypter():
             file.save(os.path.join(UPLOAD_FOLDER, file.filename))
             return render_template('file_crypter.html', name=filename, listdir=os.listdir(UPLOAD_FOLDER) )
         else:
-            return render_template('file_crypter.html', error="file not supported" )
+            return render_template('file_crypter.html', error="File not supported." )
+    else:
+        files, enc = get_uploaded_files()
 
-    return render_template('file_crypter.html')
+    return render_template('file_crypter.html', listdir=files, enc_files=enc)
 
 @routes.route('/encrypt/', methods=['GET', 'POST'])
 def encrypt():
@@ -68,19 +79,20 @@ def encrypt():
     Retorna o nome dos ficheiros apagados
     """
 
-    print(UPLOAD_FOLDER)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        print(request.form, file=sys.stderr)
+    
     listdir = os.listdir(UPLOAD_FOLDER)
-    print(listdir)
     res = []
     for f in listdir:
         print(f)
         try:
-            os.remove(os.path.join(UPLOAD_FOLDER, f))
             res.append(f)
         except:
             print("failed to delete file: " + f)
         
-    return render_template('file_crypter.html', data=res)
+    return redirect('/crypter/')
 
 @routes.route('/password_generator/', methods=['GET'])
 def password_generator():
@@ -101,7 +113,24 @@ def generate(bits=256):
     
     return render_template('password_gen.html',  data=data)
 
-def do_stuff():
-    # do whatever you need to do
-    response = "This is response from Python backend"
-    return response
+# Deleting files
+
+def delete_file():
+    print("soon")
+
+# Base 64 encoding
+
+def encode_file(): 
+    print("soon")
+# Gets uploaded files and encrypted files, returns a tuple of lists (encrypted ones and non-encrypted)
+
+def get_uploaded_files():
+    listdir = os.listdir(UPLOAD_FOLDER)
+    res = []
+    enc = []
+    for f in listdir:
+        if '.dec' in f:
+            enc.append(f)
+        else:
+            res.append(f)
+    return res, enc
